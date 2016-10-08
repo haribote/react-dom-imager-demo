b948a9c552c658000ef3f3ff696e16675f5eaebe
KIMURA Tetsuro <haribote.nobody@gmail.com>
2016-10-08 09:54:39 +0900

    create BaseComponent.

--- src/CommonComponent.js
+++ src/CommonComponent.js
@@ -1,9 +1,24 @@
 import { Component, PropTypes } from 'react';
 
 /**
+ * BaseComponent Class
+ */
+export class BaseComponent extends Component {
+  /**
+   * @method bind context to self
+   * @param methodNames
+   */
+  bindContextAll(...methodNames) {
+    methodNames.forEach((methodName) => {
+      this[methodName] = this[methodName].bind(this);
+    });
+  }
+}
+
+/**
  * CommonComponent class
  */
-export default class CommonComponent extends Component {
+export default class CommonComponent extends BaseComponent {
   /**
    * @static propTypes
    * @returns {{dispatch: *, actions: *}}
@@ -13,15 +28,5 @@ export default class CommonComponent extends Component {
       dispatch: PropTypes.func,
       actions : PropTypes.objectOf(PropTypes.func)
     }
-  }
-
-  /**
-   * @method bind context to self
-   * @param methodNames
-   */
-  bindContextAll(...methodNames) {
-    methodNames.forEach((methodName) => {
-      this[methodName] = this[methodName].bind(this);
-    });
   }
 }
